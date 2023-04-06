b96122957cb3b30dcdbca98e585921875ed3d5b3
Evgenii <w1djer@mail.ru>
2023-04-06 17:45:44 +0300

    Задача 64

---
+++ 64/Program.cs
@@ -0,0 +1,9 @@
+﻿string Function(int a, int b)
+{
+    if (a == b) return a.ToString();
+    return Function(a + 1, b) + ", " + a.ToString();
+}
+
+Console.WriteLine("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write(Function(1, x));
